8430caf064d7f308264b4dcc544f4ccf9cb8e6c4
ehnvik <gustav.ehnvik@gmail.com>
2023-09-27 19:02:30 +0200

    Created footer and products page

--- functions.php
+++ functions.php
@@ -10,6 +10,7 @@ add_action('wp_enqueue_scripts', 'easyshop_register_styles');
 function easyshop_register_scripts()
 {
     wp_enqueue_script('easyshop-bootstrap', "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js", array(), '5.3.2', true);
+    wp_enqueue_script('easyshop-fontawesome', "https://kit.fontawesome.com/e8ced85d7a.js", array(), '', true);
 }
 
 add_action('wp_enqueue_scripts', 'easyshop_register_scripts');
@@ -30,3 +31,39 @@ function easyshop_menus()
 }
 
 add_action('init', 'easyshop_menus');
+
+
+
+function easyshop_widgets()
+{
+    register_sidebar(array(
+        'name' => __('Footer Area One'),
+        'id' => 'footer_area_one',
+        'description' => __('Footer Area One'),
+        'before_widget' => '<div>',
+        'after_widget' => '</div>',
+        'before_title' => '<h4>',
+        'after_title' => '</h4>',
+    ));
+
+    register_sidebar(array(
+        'name' => __('Footer Area Two'),
+        'id' => 'footer_area_two',
+        'description' => __('Footer Area Two'),
+        'before_widget' => '<div>',
+        'after_widget' => '</div>',
+        'before_title' => '<h4>',
+        'after_title' => '</h4>',
+    ));
+
+    register_sidebar(array(
+        'name' => __('Footer Area Three'),
+        'id' => 'footer_area_three',
+        'description' => __('Footer Area Three'),
+        'before_widget' => '<div>',
+        'after_widget' => '</div>',
+        'before_title' => '<h4>',
+        'after_title' => '</h4>',
+    ));
+}
+add_action('widgets_init', 'easyshop_widgets');
