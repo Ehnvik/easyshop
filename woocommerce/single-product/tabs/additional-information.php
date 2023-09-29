a6025d3f4c7f2653102bf3a9475b2281fc91cbfa
ehnvik <gustav.ehnvik@gmail.com>
2023-09-29 17:19:44 +0200

    Styled create a review field

--- woocommerce/single-product/tabs/additional-information.php
+++ woocommerce/single-product/tabs/additional-information.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Additional Information tab
  *
@@ -15,16 +16,16 @@
  * @version 3.0.0
  */
 
-defined( 'ABSPATH' ) || exit;
+defined('ABSPATH') || exit;
 
 global $product;
 
-$heading = apply_filters( 'woocommerce_product_additional_information_heading', __( 'Additional information', 'woocommerce' ) );
+$heading = apply_filters('woocommerce_product_additional_information_heading', __('Additional information', 'woocommerce'));
 
 ?>
 
-<?php if ( $heading ) : ?>
-	<h2><?php echo esc_html( $heading ); ?></h2>
+<?php if ($heading) : ?>
+	<h2><?php echo esc_html($heading); ?></h2>
 <?php endif; ?>
 
-<?php do_action( 'woocommerce_product_additional_information', $product ); ?>
+<?php do_action('woocommerce_product_additional_information', $product); ?>
