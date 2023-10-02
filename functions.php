527f0134ae35f2fdd6d0ffe3c0bc70bd34a1ca34
Your Name <sebastian.bjelkstam@gmail.com>
2023-10-02 09:52:02 +0200

    add custom post type to news

--- functions.php
+++ functions.php
@@ -19,3 +19,56 @@ add_action('after_setup_theme', 'mytheme_add_woocommerce_support');
 }
 add_filter('excerpt_length', 'my_excerpt_length');
 ?>
+
+
+<!--  -->
+
+<?php
+
+
+function post_type_news()
+{
+    $supports = array(
+        'title',
+        'editor',
+        'author',
+        'thumbnail',
+        'excerpt',
+        'custom-fields',
+        'comments',
+        'revisions',
+        'post-formats',
+    );
+
+    $labels = array(
+        'name' => _x('News', 'plural'),
+        'singular_name' => _x('News', 'singular'),
+        'menu_name' => _x('News', 'admin menu'),
+        'name_admin_bar' => _x('News', 'admin bar'),
+        'add_new' => _x('Add New', 'add new'),
+        'add_new_item' => __('Add New news'),
+        'new_item' => __('New news'),
+        'edit_item' => __('Edit news'),
+        'view_item' => __('View news'),
+        'all_items' => __('All news'),
+        'search_items' => __('Search news'),
+        'not_found' => __('No news found.'),
+    );
+
+    $args = array(
+        'supports' => $supports, // Vilka "content" delar som ska användas i post-typen
+        'labels' => $labels, // Namn och text som syns i UI:t
+        'public' => true, // Om alla användare ska kunna skapa denna post-types
+        'query_var' => true, // Skapa en query-variabel för post-typen
+        'rewrite' => array('slug' => 'news'), // Hur man når post-typen (t.ex. som inläggsida) http://localhost/news/
+        'has_archive' => true, // Ska post-typen ha arkiv-sida? Likt inlägg
+        'hierarchical' => false, // Ska de behandlas som sidor (true) eller inlägg (false)?
+    );
+
+    register_post_type('news', $args);
+}
+
+add_action('init', 'post_type_news');
+
+
+?>
--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <?php
     if (have_posts()) :
         while (have_posts()) : the_post();
-            get_template_part('template-parts/content', 'news')
+            get_template_part('template-parts/content', 'news');
     ?>
     <?php
         endwhile;
