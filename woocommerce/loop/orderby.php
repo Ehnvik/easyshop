b34655ee7f85658b2b503521b235d480bb2e1267
ehnvik <gustav.ehnvik@gmail.com>
2023-10-03 18:54:17 +0200

    Styled orderby on shop page

--- woocommerce/loop/orderby.php
+++ woocommerce/loop/orderby.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Show options for ordering
  *
@@ -15,17 +16,19 @@
  * @version     3.6.0
  */
 
-if ( ! defined( 'ABSPATH' ) ) {
+if (!defined('ABSPATH')) {
 	exit;
 }
 
 ?>
-<form class="woocommerce-ordering" method="get">
-	<select name="orderby" class="orderby" aria-label="<?php esc_attr_e( 'Shop order', 'woocommerce' ); ?>">
-		<?php foreach ( $catalog_orderby_options as $id => $name ) : ?>
-			<option value="<?php echo esc_attr( $id ); ?>" <?php selected( $orderby, $id ); ?>><?php echo esc_html( $name ); ?></option>
-		<?php endforeach; ?>
-	</select>
-	<input type="hidden" name="paged" value="1" />
-	<?php wc_query_string_form_fields( null, array( 'orderby', 'submit', 'paged', 'product-page' ) ); ?>
-</form>
+<div class="ordering-wrapper">
+	<form class="woocommerce-ordering" method="get">
+		<select name="orderby" class="orderby" aria-label="<?php esc_attr_e('Shop order', 'woocommerce'); ?>">
+			<?php foreach ($catalog_orderby_options as $id => $name) : ?>
+				<option value="<?php echo esc_attr($id); ?>" <?php selected($orderby, $id); ?>><?php echo esc_html($name); ?></option>
+			<?php endforeach; ?>
+		</select>
+		<input type="hidden" name="paged" value="1" />
+		<?php wc_query_string_form_fields(null, array('orderby', 'submit', 'paged', 'product-page')); ?>
+	</form>
+</div>
