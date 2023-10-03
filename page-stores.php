26e16bcdfa1305fa324f0391e6f883db1a19504b
Your Name <sebastian.bjelkstam@gmail.com>
2023-10-03 15:42:06 +0200

    Custom post type add store

---
+++ page-stores.php
@@ -0,0 +1,15 @@
+<?php
+get_header();
+?>
+    <?php
+    if (have_posts()) :
+        while (have_posts()) : the_post();
+            the_title();
+            the_content();
+        endwhile;
+    endif;
+    ?>
+
+<?php
+get_footer();
+?>
