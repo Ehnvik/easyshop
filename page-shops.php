1430230929ce0ef369280e3f07d32d7cc539c16e
Your Name <sebastian.bjelkstam@gmail.com>
2023-10-04 10:49:17 +0200

    custom post type stores

--- page-shops.php
+++ page-shops.php
@@ -2,8 +2,14 @@
 get_header();
 ?>
 
-<a href="<?php the_permalink("http://localhost/easyshop/wordpress/shops/store1/") ?>">Affär1</a>
+<a href="<?php the_permalink("http://localhost/easyshop3/wordpress/shops/store1/") ?>">Affär1</a>
 <a href="<?php the_permalink("http://localhost/easyshop/wordpress/shops/store2/") ?>">Affär2</a>
+<a href="<?php the_permalink("http://localhost/easyshop3/wordpress/shops/store3/") ?>">Affär3</a>
+<a href="<?php the_permalink("http://localhost/easyshop3/wordpress/shops/store4/") ?>">Affär4</a>
+
+
+
+
 
 <?php
 if (have_posts()) :
